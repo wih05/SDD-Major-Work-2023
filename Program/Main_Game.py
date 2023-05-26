07bc7871cb0d7f26675e20f318fac01de39f41b2
wih05 <74883043+wih05@users.noreply.github.com>
2023-05-26 12:36:31 +1000

    Update Main_Game.py
    Added background for text in levels, endless, and help screen. as well as added text to the help screen.

--- Program/Main_Game.py
+++ Program/Main_Game.py
@@ -7,6 +7,8 @@ import button
 #--         --#
 
 #-- Setup --#
+pygame.init()
+
 # Window dimensions
 Screen_Height = 730
 Screen_Width = 1200
@@ -14,6 +16,10 @@ Screen_Width = 1200
 # Window creation
 screen = pygame.display.set_mode((Screen_Width, Screen_Height))
 pygame.display.set_caption('Kanji Fighters') # WIP name
+
+# Text Setup
+White = (255, 255, 255) # RGB value of white
+Pixil_Font = pygame.font.SysFont('consolas', 30, True) # Font setup, Font: consolas, size 30, Bold
 
 # Load images
 Levels_img = pygame.image.load('Program\Sprites\Menu_Levels_Button.png').convert_alpha()
@@ -23,6 +29,9 @@ Quit_img = pygame.image.load('Program\Sprites\Menu_Quit_Button.png').convert_alp
 Levels_S_img = pygame.image.load('Program\Sprites\Menu_Levels_Button_S.png').convert_alpha()
 Endless_S_img = pygame.image.load('Program\Sprites\Menu_Endless_Button_S.png').convert_alpha()
 Help_S_img = pygame.image.load('Program\Sprites\Menu_Help_Button_S.png').convert_alpha()
+Menu_Text_BG = pygame.image.load('Sprites\Menu_Text_BG.png').convert_alpha()
+Menu_Text_BG = pygame.transform.scale(Menu_Text_BG, (900, 600)) # Scales IMG up by 2
+
 # Buttons
 Levels_Button = button.Button(Levels_img, 2)
 Endless_Button = button.Button(Endless_img, 2)
@@ -34,6 +43,14 @@ Help_Button_S = button.Button(Help_S_img, 2)
 
 
 game_state = 'Main_Menu'
+
+#--       --#
+
+#-- Functions --#
+def Display_Text(Surface, Text, Font, Colour, x, y): # function to display text on screen
+    text = Font.render(Text, True, Colour) # creates the text
+    Surface.blit(text, (x, y)) # displays text
+#--           --#
 
 #-- Main Loop --#
 run = True
@@ -59,6 +76,7 @@ while run:
             game_state = 'Help'
         if Quit_Button.draw(50, 620, screen):
             run = False
+        screen.blit(Menu_Text_BG, (250, 100))
 
     if game_state == 'Endless':
         if Levels_Button.draw(50, 100, screen):
@@ -68,6 +86,7 @@ while run:
             game_state = 'Help'
         if Quit_Button.draw(50, 620, screen):
             run = False
+        screen.blit(Menu_Text_BG, (250, 100))
 
     if game_state == 'Help':
         if Levels_Button.draw(50, 100, screen):
@@ -77,6 +96,25 @@ while run:
         Help_Button_S.draw(50, 440, screen)
         if Quit_Button.draw(50, 620, screen):
             run = False
+        screen.blit(Menu_Text_BG, (250, 100))
+        Display_Text(screen, "Help:", Pixil_Font, White, 260, 105)
+        Display_Text(screen, "To start select either 'Levels' or 'Endless'", Pixil_Font, White, 260, 140)
+        Display_Text(screen, "from there select a level, or difficulty, then", Pixil_Font, White, 260, 170)
+        Display_Text(screen, "press start and begin the battle.", Pixil_Font, White, 260, 195)
+        Display_Text(screen, "In Battle Help:", Pixil_Font, White, 260, 255)
+        Display_Text(screen, "In battle, to attack select an option from the", Pixil_Font, White, 260, 290)
+        Display_Text(screen, "'attack' area, in order to deal damage however,", Pixil_Font, White, 260, 320)
+        Display_Text(screen, "you must select the option correlating to the", Pixil_Font, White, 260, 350)
+        Display_Text(screen, "enemy kanji. this may be either the correct readings", Pixil_Font, White, 260, 380)
+        Display_Text(screen, "or definition of the kanji.", Pixil_Font, White, 260, 410)
+        Display_Text(screen, "In some cases, the enemy kanji may have multiple", Pixil_Font, White, 260, 440)
+        Display_Text(screen, "characters, to make up a word. In that case", Pixil_Font, White, 260, 470)
+        Display_Text(screen, "the method of attack remains the same, using either", Pixil_Font, White, 260, 500)
+        Display_Text(screen, "the reading of the kanji when in the word or", Pixil_Font, White, 260, 530)
+        Display_Text(screen, "the definition of the word itself.", Pixil_Font, White, 260, 560)
+        Display_Text(screen, "Finally once you finish, the next level will be", Pixil_Font, White, 260, 590)
+        Display_Text(screen, "unlocked, or highscore added to 'Endless' screen.", Pixil_Font, White, 260, 620)
+        Display_Text(screen, "Finally, have fun!", Pixil_Font, White, 260, 650)
 
     for event in pygame.event.get(): # Checks for event occuring
         
